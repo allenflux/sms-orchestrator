a24f864f95401d1e9b9f164e67dd2966be3e2c3a
Allen <Allen@allen.com>
2024-12-07 16:07:15 +0700

    Feat : init Project

---
+++ api/v1/career/career_device_management.go
@@ -0,0 +1,51 @@
+package career
+
+import "github.com/gogf/gf/v2/frame/g"
+
+// Register with the platform
+
+type RegisterReq struct {
+	g.Meta       `path:"/device" tags:"Device使用的API" method:"post" dc:"注册设备到平台" `
+	DeviceNumber string `json:"device_number" dc:"设备号" v:"required"`
+	PhoneNumber  string `json:"phone_number" dc:"设备手机号" v:"required"`
+	ActiveTime   string `json:"active_time" dc:"激活时间 时间戳" v:"required"`
+}
+type RegisterRes struct{}
+
+// Reporting Device Information
+
+type ReportInfoReq struct {
+	g.Meta       `path:"/device/info" tags:"Device使用的API" method:"post" dc:"根据设备号获取设备信息"`
+	DeviceNumber string `json:"device_number" dc:"设备号" v:"required"`
+}
+type ReportInfoRes struct{}
+
+// Fetch Task
+
+type FetchTaskReq struct {
+	g.Meta       `path:"/device/task" tags:"Device使用的API" method:"post" dc:"根据设备号获取设备任务"`
+	DeviceNumber string `json:"device_number" dc:"设备号" v:"required"`
+}
+type FetchTaskRes struct{}
+
+// Report Task Results
+
+type ReportTaskResultReq struct {
+	g.Meta       `path:"/device/task/result" tags:"Device使用的API" method:"post" dc:"根据设备号上报任务执行结果"`
+	DeviceNumber string `json:"device_number" dc:"设备号" v:"required"`
+	SendStatus   int    `json:"send_status" dc:"1-成功 2-失败" v:"required"`
+	Reason       string `json:"reason" dc:"如果不成功告知失败原因"`
+}
+type ReportTaskResultRes struct{}
+
+// Report Receive Content
+
+type ReportReceiveContentReq struct {
+	g.Meta            `path:"/device/task/content" tags:"Device使用的API" method:"post" dc:"根据设备号上报接收的内容"`
+	DeviceNumber      string `json:"device_number" dc:"设备号" v:"required"`
+	SmsContent        string `json:"sms_content" v:"required"`
+	SmsTopic          string `json:"sms_topic" v:"required"`
+	ReceiveTime       string `json:"receive_time" v:"required" dc:"接收时间戳"`
+	TargetPhoneNumber string `json:"target_phone_number" v:"required" dc:"接收的手机号"`
+}
+type ReportReceiveContentRes struct{}
--- api/v1/sms/sub_controller_sms_management.go
+++ api/v1/sms/sub_controller_sms_management.go
@@ -2,6 +2,7 @@ package sms
 
 import (
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
 	commonApi "sms_backend/api/v1/common"
 	"sms_backend/internal/model"
 )
@@ -28,6 +29,60 @@ type SubTaskListRes struct {
 		QuantitySent      string `json:"quantity_sent" dc:"以发送数量"`
 		AssociatedAccount string `json:"associated_account" dc:"所属子账号"`
 		IntervalTime      string `json:"interval_time" dc:"间隔时间"`
+		StartTime         string `json:"start_time" dc:"开始时间"`
+		CreateTime        string `json:"create_time" dc:"创建时间"`
+	} `json:"data"`
+}
+
+// Create Task
+type SubTaskCreateReq struct {
+	g.Meta          `path:"/sub/task" mine:"multipart/form-data" tags:"子平台群发短信" method:"post" dc:"创建任务" `
+	File            *ghttp.UploadFile `json:"file" v:"required"`
+	TaskName        string            `json:"task_name" v:"required"`
+	GroupID         int               `json:"group_id" dc:"选择分组" v:"required"`
+	IntervalTime    string            `json:"interval_time" dc:"间隔时间" v:"required"`
+	TimingStartTime string            `json:"timing_start_time"  dc:"定时启动时间"`
+}
+
+type SubTaskCreateRes struct{}
+
+// Recall Task
+
+type SubTaskDeleteReq struct {
+	g.Meta `path:"/sub/task"  tags:"子平台群发短信" method:"delete" dc:"撤销任务" `
+	TaskID int `json:"task_id" v:"required" dc:"任务id"`
+}
+
+type SubTaskDeleteRes struct{}
+
+// 群发记录
+
+type SubTaskRecordReq struct {
+	model.PageReq
+	g.Meta            `path:"/sub/task/record" tags:"群发记录" method:"get" dc:"查看群发短信列表" `
+	ProjectID         int      `json:"project_id" dc:"所属项目id" example:"1"`
+	SmsStatus         string   `json:"sms_status" dc:"SMS Status 发送状态"`
+	TaskName          string   `json:"task_name" dc:"任务名称"`
+	TargetPhoneNumber string   `json:"target_phone_number" dc:"Target Phone Number 目标手机号"`
+	DeviceNumber      string   `json:"device_number" dc:"Device Number 执行设备号"`
+	SentDateRange     []string `json:"sent_date_range" p:"dateRange 1" description:"发送日期范围"`
+	CreateDateRange   []string `json:"create_date_range" p:"dateRange 2" description:"创建日期范围"`
+}
+
+type SubTaskRecordRes struct {
+	commonApi.ListRes
+	Data struct {
+		ID                int    `json:"id" dc:"序号"`
+		TaskName          string `json:"task_name" dc:"任务名称"`
+		SubTaskID         string `json:"sub_task_id" dc:"Sub Task ID 子任务ID"`
+		TargetPhoneNumber string `json:"target_phone_number" dc:"Target Phone Number 目标手机号"`
+		DeviceNumber      string `json:"device_number" dc:"Device Number 执行设备号"`
+		SmsTopic          string `json:"sms_topic" dc:"SMS Topic 主题"`
+		SmsContent        string `json:"sms_content" dc:"SMS Content 短信内容"`
+		SmsStatus         string `json:"sms_status" dc:"SMS Status 短信发送状态"`
+		AssociatedAccount string `json:"associated_account" dc:"所属子账号"`
+		ProjectName       string `json:"project_name" dc:"Project Name 所属项目"`
+		Note              string `json:"note" dc:"失败原因"`
 		StartTime         string `json:"start_time" dc:"开始时间"`
 		CreateTime        string `json:"create_time" dc:"创建时间"`
 	} `json:"data"`
