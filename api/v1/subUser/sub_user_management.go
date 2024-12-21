96bc395f3003cb36368f54f6f3e13d022e8c3fa1
David <mengxing002@gmail.com>
2024-12-21 09:52:50 +0700

    refactor(subUser): 重构子账号相关接口并对接服务层- 更新了 SubRegisterReq 结构体字段名称 - 移除了子账号相关控制器中的未实现代码 - 对接了服务层方法，包括变更状态、删除子账号、获取列表、注册和更新子账号

--- api/v1/subUser/sub_user_management.go
+++ api/v1/subUser/sub_user_management.go
@@ -9,9 +9,9 @@ import (
 
 type SubRegisterReq struct {
 	g.Meta   `path:"/register" tags:"子账号后台(admin)" method:"post" sm:"添加子账号"`
-	Username string                 `json:"username" v:"passport" dc:"用户名"`
+	Name     string                 `json:"name" v:"passport" dc:"用户名"`
 	Password string                 `json:"password" v:"password" dc:"密码"`
-	Project  []int                  `json:"project" v:"required" dc:"项目id，不分配则提交空数组"`
+	Project  []int                  `json:"project" dc:"项目id，不分配则提交空数组"`
 	Status   consts.EnumsUserStatus `json:"status" v:"enums" dc:"状态,1-停用 2-启用"`
 	Note     string                 `json:"note" dc:"备注"`
 }
